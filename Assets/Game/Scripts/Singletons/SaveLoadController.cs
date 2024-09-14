dff1e5ce7a96a33b87fb4a0f3e91945f41cbf4d0
Leonardo Ferrari Fraga <Leonardo_.L@hotmail.com>
2024-09-14 19:42:38 -0300

    Add SaveLoad controller

---
+++ Assets/Game/Scripts/Singletons/SaveLoadController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class SaveLoadController : MonoBehaviour
+{
+    public static SaveLoadController instance;
+
+    [SerializeField] private IntData _levelData;
+    [SerializeField] private IntData _highscoreData;
+    
+    private int lastSavedIndex;
+    private int highscore;
+
+    public int Level => lastSavedIndex;
+    public int Highscore => highscore;
+    
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+        
+        lastSavedIndex = PlayerPrefs.GetInt(SaveLoadParameters.LAST_LEVEL_PLAYED, 0);
+        highscore = PlayerPrefs.GetInt(SaveLoadParameters.HIGHSCORE, 0);
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt(SaveLoadParameters.LAST_LEVEL_PLAYED, _levelData.value);
+        PlayerPrefs.SetInt(SaveLoadParameters.HIGHSCORE, _highscoreData.value);
+        lastSavedIndex = _levelData.value;
+        highscore = _highscoreData.value;
+    }
+}
+
+public static class SaveLoadParameters
+{
+    public const string LAST_LEVEL_PLAYED = "llp";
+    public const string HIGHSCORE = "hs";
+}
